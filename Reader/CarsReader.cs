ec7e32c923975916404e56a6e217f065b90281dc
421p <andrew.bakal@gmail.com>
2016-11-28 03:30:42 +0200

    car? car!

---
+++ Reader/CarsReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using git_exam.Person;
+using git_exam.Vehicles;
+using YamlDotNet.Serialization;
+
+namespace git_exam.Reader {
+    public class CarsReader : AbstractGetter<List<Car>> {
+        public override List<Car> Get()
+        {
+            var deserializer = new Deserializer();
+            return deserializer.Deserialize<List<Car>>(File.ReadAllText("../../cars.yml"));
+        }
+    }
+}
---
+++ Vehicles/Car.cs
@@ -0,0 +1,10 @@
+﻿using YamlDotNet.Serialization;
+
+namespace git_exam.Vehicles {
+    public class Car {
+        [YamlMember(Alias = "model")]
+        public string Model { get; set; }
+        [YamlMember(Alias = "price")]
+        public decimal Price { get; set; }
+    }
+}
